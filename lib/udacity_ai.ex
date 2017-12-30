7e023482431b5cd2e41e72f51184e5c3cd32c197
Mario Uher <uher.mario@gmail.com>
2017-12-30 09:12:39 +0100

    Implement questions 1-3

--- lib/udacity_ai.ex
+++ lib/udacity_ai.ex
@@ -1,18 +1,48 @@
 defmodule UdacityAi do
-  @moduledoc """
-  Documentation for UdacityAi.
-  """
+  @coin [:H, :T]
+  @coins for c1 <- @coin,
+             c2 <- @coin,
+             c3 <- @coin,
+             do: [c1, c2, c3]
 
   @doc """
-  Hello world.
+  The probability of flipping a fair coin (i.e., p(Heads)=0.5) three times and
+  observing the EXACT sequence {Heads, Heads, Tails} is:
 
-  ## Examples
-
-      iex> UdacityAi.hello
-      :world
-
+  iex> UdacityAi.question_1
+  0.125
   """
-  def hello do
-    :world
+  def question_1 do
+    coins_probability(fn coins -> coins == [:H, :H, :T] end)
+  end
+
+  @doc """
+  The probability of flipping a fair coin (i.e., p(Heads)=0.5) three times and
+  observing AT LEAST two Heads is:
+
+  iex> UdacityAi.question_2
+  0.5
+  """
+  def question_2 do
+    coins_probability(fn coins ->
+      Enum.count(coins, fn coin -> coin == :H end) >= 2
+    end)
+  end
+
+  @doc """
+  The probability of flipping a fair coin (i.e., p(Heads)=0.5) three times and
+  observing EXACTLY two Heads and one Tails is:
+
+  iex> UdacityAi.question_3
+  0.375
+  """
+  def question_3 do
+    coins_probability(fn coins ->
+      Enum.count(coins, fn coin -> coin == :H end) == 2
+    end)
+  end
+
+  defp coins_probability(fun) do
+    Enum.count(@coins, fun) / Enum.count(@coins)
   end
 end
--- test/udacity_ai_test.exs
+++ test/udacity_ai_test.exs
@@ -1,8 +1,4 @@
 defmodule UdacityAiTest do
   use ExUnit.Case
   doctest UdacityAi
-
-  test "greets the world" do
-    assert UdacityAi.hello() == :world
-  end
 end
